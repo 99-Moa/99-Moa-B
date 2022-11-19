cf27e68e34c7ff706a59374b8c8fe87f532d8017
acutecritic <acutecritic@naver.com>
2022-11-19 22:23:44 +0900

    fix : 카카오 로그인 토큰완료!

--- src/main/java/com/moa/moabackend/kakaoLogin/SocailLoginController.java
+++ src/main/java/com/moa/moabackend/kakaoLogin/SocailLoginController.java
@@ -5,6 +5,7 @@ import com.moa.moabackend.entity.user.SocialUserInfoDto;
 import com.moa.moabackend.jwt.TokenDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +17,7 @@ public class SocailLoginController {
 
     // 카카오 로그인
     @GetMapping("/kakao")
-    public SocialUserInfoDto kakaoLogin(String code, TokenDto tokenDto, HttpServletResponse response) throws JsonProcessingException {
+    public SocialUserInfoDto kakaoLogin(@RequestParam(name="code") String code, TokenDto tokenDto, HttpServletResponse response) throws JsonProcessingException {
         return kakaoUserService.kakaoLogin(code, tokenDto, response);
     }
 }
