87089bda17ed7901fcf66f7c21262823b85dce90
acutecritic <acutecritic@naver.com>
2022-11-28 16:44:14 +0900

    fix

--- src/main/java/com/moa/moabackend/kakaoLogin/KakaoUserService.java
+++ src/main/java/com/moa/moabackend/kakaoLogin/KakaoUserService.java
@@ -80,7 +80,7 @@ public class KakaoUserService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "763b280756ce16575365712d2a5ba668");
-        body.add("redirect_uri", "http://localhost:3000/kakao");
+        body.add("redirect_uri", "https://main.d36xaay1mw1kl0.amplifyapp.com/kakao");
         body.add("code", code);
 
         // HTTP 요청 보내기
