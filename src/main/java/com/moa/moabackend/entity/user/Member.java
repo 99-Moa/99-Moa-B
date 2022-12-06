b378f461ea5f37707c46672f671028d0af918be4
DESKTOP-7T6G0VP\신현재 <tmpanmitw2021@naver.com>
2022-12-06 17:18:22 +0900

    no message

--- src/main/java/com/moa/moabackend/entity/user/Member.java
+++ src/main/java/com/moa/moabackend/entity/user/Member.java
@@ -4,27 +4,37 @@ package com.moa.moabackend.entity.user;
 import com.moa.moabackend.entity.Timestamped;
 import com.moa.moabackend.entity.user.mypage.MypageRequestDto;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
+
 @Entity
-@Table(name = "users")  // table name 은 'user' 가 아닌 'users' 로 변경
+@Getter
+@Setter
+@Table(name = "member")  // table name 은 'user' 가 아닌 'users' 로 변경
 @NoArgsConstructor
 public class User extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 //    @NotBlank
+//    @Column
     private String userId;
 //    @NotBlank
+//    @Column
     private String password;
 //    @NotBlank
+//    @Column
     private String passwordCheck;
 //    @NotBlank
+//    @Column
     private String userName;
 
+//    @Column
     private String imgUrl = "https://yuns8708bucket.s3.ap-northeast-2.amazonaws.com/images/Icon_Profile.png";
 
     public User(UserRequestDto userRequestDto) {
